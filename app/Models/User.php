688ff369dd32c36af8feee9d100c29a469d15bd5
danilo.bossanova <danilo.bossanova@hotmail.com>
2025-02-05 19:41:22 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -29,6 +30,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'congregacao_id',
     ];
 
     /**
@@ -64,4 +66,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * Obtém a congregação do usuário
+     */
+    public function congregacao(): BelongsTo
+    {
+        return $this->belongsTo(Congregacao::class, 'congregacao_id');
+    }
+
 }
