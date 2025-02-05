aa5e8d0ff003669fff8eb367a122d17570b49e6b
danilo.bossanova <danilo.bossanova@hotmail.com>
2025-02-05 19:40:55 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CongregacoesSeed::class, // Primeiro o seeder de Congregações
+            UserSeed::class,         // Depois o seeder de Usuários
+            DiscursosSeeder::class  // Discursos
         ]);
     }
 }
