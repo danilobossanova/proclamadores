0b0fca7260951dc1b543d0e67f6162745d04ae95
danilo.bossanova <danilo.bossanova@hotmail.com>
2025-02-10 22:10:46 -0300

    Update EditarCongregacao.php

--- app/Livewire/Congregacoes/EditarCongregacao.php
+++ app/Livewire/Congregacoes/EditarCongregacao.php
@@ -7,34 +7,60 @@ use Livewire\Component;
 
 class EditarCongregacao extends Component
 {
-    public Congregacao $congregacao;
+    public $congregacao_id;
+    public $congregacao;
+    public $responsavel;
+    public $telefone;
+    public $dia_reuniao_fds;
+    public $hora_reuniao;
+    public $endereco;
+    public $coordenadas;
+    public $googlemaps;
 
-    protected function rules()
+    public function mount($id)
     {
-        return [
-            'congregacao' => 'required|min:3|max:255|unique:congregacoes,nome',
-            'responsavel' => 'required|min:2|max:100',
-            'telefone' => 'required',
-            'dia_reuniao_fds' => 'required',
-            'hora_reuniao' => 'required',
-            'endereco' => 'required|min:5|max:255',
-            'coordenadas' => 'required',
-            'googlemaps' => 'required'
-        ];
-    }
+        $congregacao = Congregacao::findOrFail($id);
 
-    public function mount(Congregacao $congregacao)
-    {
-        $this->congregacao = $congregacao;
+        // Popula as propriedades com os valores do banco de dados
+        $this->congregacao_id = $congregacao->id;
+        $this->congregacao = $congregacao->congregacao;
+        $this->responsavel = $congregacao->responsavel;
+        $this->telefone = $congregacao->telefone;
+        $this->dia_reuniao_fds = $congregacao->dia_reuniao_fds;
+        $this->hora_reuniao = $congregacao->hora_reuniao;
+        $this->endereco = $congregacao->endereco;
+        $this->coordenadas = $congregacao->coordenadas;
+        $this->googlemaps = $congregacao->googlemaps;
     }
 
     public function salvar()
     {
-        $this->validate();
-        $this->congregacao->save();
+        $this->validate([
+            'congregacao' => 'required|string|max:255',
+            'responsavel' => 'required|string|max:255',
+            'telefone' => 'required|string|max:15',
+            'dia_reuniao_fds' => 'required|string',
+            'hora_reuniao' => 'required',
+            'endereco' => 'required|string|max:500',
+            'coordenadas' => 'nullable|string',
+            'googlemaps' => 'nullable|url'
+        ]);
+
+        $congregacao = Congregacao::findOrFail($this->congregacao_id);
+
+        // Atualiza os dados no banco
+        $congregacao->update([
+            'congregacao' => $this->congregacao,
+            'responsavel' => $this->responsavel,
+            'telefone' => $this->telefone,
+            'dia_reuniao_fds' => $this->dia_reuniao_fds,
+            'hora_reuniao' => $this->hora_reuniao,
+            'endereco' => $this->endereco,
+            'coordenadas' => $this->coordenadas,
+            'googlemaps' => $this->googlemaps,
+        ]);
 
         session()->flash('message', 'Congregação atualizada com sucesso!');
-        return redirect()->route('congregacoes.index');
     }
 
     public function render()
